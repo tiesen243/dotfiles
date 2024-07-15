bba810a197afb391fb5b9f92cfe2f25cc7e17919
tiesen243 <ttien56906@gmail.com>
2024-07-15 17:29:29 +0700

    chore: update commit function

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,7 +3,7 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="dracula"
 
 # Plugins
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(zsh-autosuggestions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -67,7 +67,7 @@ alias spec="fastfetch -c ~/dotfiles/zsh/fastfetch.jsonc"
 
 gP () {
   git add .
-  git commit -m "$1"
+  git commit
   git push
 }
 
