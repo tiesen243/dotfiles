f1a4999c81f62c392e298e96ce848b3fbb33d7aa
tiesen243 <ttien56906@gmail.com>
2024-10-14 14:45:53 +0700

    feat(readme): simplify installation instructions
    - Removed manual linking of zsh theme and config files
    - Added a reminder to choose zsh as the default shell
    - Replaced multiple ln commands with a single install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/hypr ~/.config
+ln -s ~/dotfiles/btop ~/.config
+ln -s ~/dotfiles/kitty ~/.config
+ln -s ~/dotfiles/dunst ~/.config
+ln -s ~/dotfiles/zsh/config.zsh ~/
+ln -s ~/dotfiles/lazygit ~/.config
+ln -s ~/dotfiles/fastfetch ~/.config
+ln -s ~/dotfiles/zsh/themes/yuki.zsh-theme ~/.oh-my-zsh/custom/themes
