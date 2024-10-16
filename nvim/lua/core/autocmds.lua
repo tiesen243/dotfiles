19c2ed58b67b269b5de82f9a2422c26403d5a3ed
tiesen243 <ttien56906@gmail.com>
2024-10-17 02:23:47 +0700

    feat: update plugins and configurations
    - Updated nvim-cmp configuration to load on InsertEnter event
    - Added nvim-treesitter plugin with auto-install and sync-install
    - Added nvim-colorizer plugin for color highlighting
    - Removed redundant plugins and configurations
    - Updated key mappings for CopilotChat and Telescope
    - Improved formatting and indentation in various files

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,55 +1,56 @@
 local function augroup(name)
-  return vim.api.nvim_create_augroup("yuki_" .. name, { clear = true })
+	return vim.api.nvim_create_augroup("yuki_" .. name, { clear = true })
 end
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup("highlight_yank"),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- Automatically format on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup("format_on_save"),
+	callback = function()
+		vim.lsp.buf.format({ timeout_ms = 2000 })
+	end,
 })
 
 -- Fix conceallevel for json files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = augroup("json_conceal"),
-  pattern = { "json", "jsonc" },
-  callback = function()
-    vim.opt_local.conceallevel = 0
-  end,
+	group = augroup("json_conceal"),
+	pattern = { "json", "jsonc" },
+	callback = function()
+		vim.opt_local.conceallevel = 0
+	end,
 })
 
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("wrap_spell"),
-  pattern = { "*.txt", "*.tex", "*.typ", "gitcommit", "markdown", "mdx" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+	group = augroup("wrap_spell"),
+	pattern = { "*.txt", "*.tex", "*.typ", "gitcommit", "markdown", "mdx" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("close_with_q"),
-  pattern = {
-    "PlenaryTestPopup",
-    "help",
-    "lspinfo",
-    "notify",
-    "qf",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
-    "dbout",
-    "gitsigns.blame",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
+	group = augroup("close_with_q"),
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"notify",
+		"startuptime",
+		"checkhealth",
+		"gitsigns.blame",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
 })
