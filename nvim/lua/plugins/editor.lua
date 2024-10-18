6d9d9d1629a9493de674c265b6261029bb2b88d2
tiesen243 <ttien56906@gmail.com>
2024-10-19 05:50:52 +0700

    feat: update plugin configurations and dependencies
    - Updated nvim-cmp configuration and dependencies
    - Modified rest-nvim key mappings and configuration
    - Enhanced github-nvim-theme setup with transparent.nvim
    - Improved telescope.nvim key mappings and setup
    - Refined nvim-tree.lua configuration
    - Added which-key.nvim configuration
    - Introduced flash.nvim for enhanced navigation
    - Updated lualine.nvim configuration for status line
    - Added indent-blankline.nvim for better indentation
    - Enhanced alpha-nvim dashboard setup
    - Configured noice.nvim for better notifications
    - Updated tmux.nvim key mappings and configuration

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,323 +1,307 @@
 return {
-  {
-    "rest-nvim/rest.nvim",
-    keys = {
-      { "<C-CR>", "<cmd>Rest run<cr>", desc = "Send request" },
-    },
-    config = function()
-      ---@type rest.Opts
-      vim.g.rest_nvim = {}
-    end,
-  },
+	{
+		"rest-nvim/rest.nvim",
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "http",
+				callback = function()
+					vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>", {})
+					vim.keymap.set("n", "<leader>rc", "<cmd>Rest Cookies<cr>", {})
+				end,
+			})
+		end,
+		config = function()
+			vim.g.rest_nvim = {}
+		end,
+	},
 
-  -- Github theme --
-  {
-    "projekt0n/github-nvim-theme",
-    dependencies = { "xiyaowong/transparent.nvim" },
-    config = function()
-      require("github-theme").setup({
-        options = {
-          darken = { floats = true },
-          transparent = vim.g.transparent_enabled,
-        },
-      })
+	-- Github theme --
+	{
+		"projekt0n/github-nvim-theme",
+		dependencies = { "xiyaowong/transparent.nvim" },
+		config = function()
+			require("github-theme").setup({
+				options = {
+					darken = { floats = true },
+					transparent = vim.g.transparent_enabled,
+				},
+			})
 
-      vim.cmd([[colorscheme github_dark]])
+			require("transparent").setup({
+				extra_groups = { "NormalFloat", "NvimTreeNormal" },
+				exclude_groups = { "CursorLine", "CursorLineNr" },
+			})
 
-      local transparent = require("transparent")
+			vim.cmd([[colorscheme github_dark]])
+		end,
+	},
 
-      transparent.setup({
-        extra_groups = { "NormalFloat", "NvimTreeNormal" },
-        exclude_groups = { "CursorLine" },
-      })
+	-- Telescope --
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		keys = function()
+			local builtin = require("telescope.builtin")
+			return {
+				{ "<leader>fb", builtin.buffers, desc = "Buffers" },
+				{ "<leader>fg", builtin.live_grep, desc = "Live Grep" },
+				{ "<leader>fG", builtin.git_files, desc = "Git Files" },
+				{ "<leader>ff", builtin.find_files, desc = "Find Files" },
+				{ "<leader>fr", builtin.oldfiles, desc = "Recent Files" },
+				{ "<leader>ft", builtin.treesitter, desc = "Treesitter" },
+				{ "<leader>fe", builtin.file_browser, desc = "File Browser" },
+			}
+		end,
 
-      transparent.clear_prefix("BufferLine")
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
 
-      vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
-      vim.g.transparent_groups = vim.list_extend(
-        vim.g.transparent_groups or {},
-        vim.tbl_map(function(v)
-          return v.hl_group
-        end, vim.tbl_values(require("bufferline.config").highlights))
-      )
-    end,
-  },
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<esc>"] = actions.close,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+						},
+					},
+				},
+				pickers = {
+					find_files = { theme = "dropdown" },
+				},
+			})
 
-  -- Telescope --
-  {
-    "nvim-telescope/telescope.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-file-browser.nvim",
-    },
-    keys = {
-      { "<leader>fb", "<cmd>Telescope buffers<cr>",      desc = "Buffers" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>",    desc = "Live Grep" },
-      { "<leader>ff", "<cmd>Telescope find_files<cr>",   desc = "Find Files" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",     desc = "Recent Files" },
-      { "<leader>fe", "<cmd>Telescope file_browser<cr>", desc = "File Browser" },
-    },
+			telescope.load_extension("noice")
+			telescope.load_extension("file_browser")
+		end,
+	},
 
-    config = function()
-      local telescope = require("telescope")
-      local actions = require("telescope.actions")
+	-- Nvim Tree --
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		keys = {
+			{ "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "File explorer" },
+		},
+		config = function()
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
+			vim.opt.termguicolors = true
 
-      telescope.setup({
-        defaults = {
-          mappings = {
-            i = {
-              ["<esc>"] = actions.close,
-              ["<C-j>"] = actions.move_selection_next,
-              ["<C-k>"] = actions.move_selection_previous,
-            },
-          },
-        },
-      })
+			require("nvim-tree").setup({
+				sort = { sorter = "case_sensitive" },
+				view = { width = 32 },
+				renderer = { group_empty = true },
+				filters = {
+					enable = true,
+					dotfiles = false,
+					git_ignored = true,
+					custom = { ".git" },
+					exclude = { ".env", ".env*.local" },
+				},
+			})
+		end,
+	},
 
-      telescope.load_extension("file_browser")
-    end,
-  },
+	-- Which Key --
+	{
+		"folke/which-key.nvim",
+		config = function()
+			local wk = require("which-key")
 
-  -- Nvim Tree --
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "File explorer" },
-    },
-    config = function()
-      local status_ok, nvim_tree = pcall(require, "nvim-tree")
-      if not status_ok then
-        return
-      end
+			wk.setup({
+				---@type false | "classic" | "modern" | "helix"
+				preset = "modern",
 
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-      vim.opt.termguicolors = true
+				triggers = { "<leader>", mode = { "n" } },
 
-      nvim_tree.setup({
-        sort = { sorter = "case_sensitive" },
-        view = { width = 24 },
-        renderer = { group_empty = true },
-        filters = {
-          enable = true,
-          dotfiles = false,
-          git_ignored = true,
-          custom = { ".git" },
-          exclude = { ".env", ".env*.local" },
-        },
-      })
-    end,
-  },
+				win = {
+					row = -1,
+					title = false,
+					zindex = 1000,
+					---@type "none" | "single" | "double" | "rounded"
+					border = "rounded",
+					padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+				},
+				layout = {
+					width = { min = 20 }, -- min and max width of the columns
+					spacing = 4, -- spacing between columns
+					align = "center", -- align columns left, center or right
+				},
 
-  -- Which Key --
-  {
-    "folke/which-key.nvim",
-    config = function()
-      local wk = require("which-key")
+				plugins = { marks = false, registers = false, spelling = false },
+			})
 
-      wk.setup({
-        ---@type false | "classic" | "modern" | "helix"
-        preset = "modern",
+			wk.add({
+				{ "<leader>f", group = "Telescope" },
+				{ "<leader>l", group = "LSP", icon = { icon = " ", color = "green" } },
+				{ "<leader>o", group = "Options", icon = { icon = " ", color = "blue" } },
+				{ "<leader>c", group = "Copilot", icon = { icon = " ", color = "orange" } },
+			})
+		end,
+	},
 
-        triggers = { "<leader>", mode = { "n" } },
+	-- Flash --
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("flash").setup()
+		end,
+	},
 
-        win = {
-          row = -1,
-          title = false,
-          zindex = 1000,
-          ---@type "none" | "single" | "double" | "rounded"
-          border = "rounded",
-          padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
-        },
-        layout = {
-          width = { min = 20 }, -- min and max width of the columns
-          spacing = 4,     -- spacing between columns
-          align = "center", -- align columns left, center or right
-        },
-      })
+	-- Status Line --
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				options = {
+					disabled_filetypes = { "alpha", "NvimTree" },
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = {
+						{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+						{ "filename", file_status = true, path = 1 },
+						{ "branch" },
+						{ "diff" },
+					},
+					lualine_c = { "diagnostics" },
+					lualine_x = { "encoding", "fileformat", "rest" },
+					lualine_y = {
+						{ "progress", padding = { left = 1, right = 0 }, separator = "" },
+						{ "location", padding = { left = 0, right = 1 } },
+					},
+					lualine_z = {
+						function()
+							return " " .. os.date("%R")
+						end,
+					},
+				},
+			})
+		end,
+	},
 
-      wk.add({
-        { "<leader>f", group = "Telescope" },
-        { "<leader>l", group = "LSP", icon = { icon = " ", color = "green" } },
-        { "<leader>o", group = "Options", icon = { icon = " ", color = "blue" } },
-        { "<leader>c", group = "Copilot", icon = { icon = " ", color = "orange" } },
-      })
-    end,
-  },
+	-- Indent Blank Line --
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("ibl").setup()
+		end,
+	},
 
-  -- Buffer Line --
-  {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<Tab>",   "<cmd>BufferLineCycleNext<cr>", desc = "Next tab" },
-      { "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev tab" },
-    },
-    config = function()
-      require("bufferline").setup({
-        options = {
-          max_name_length = 15,
-          offsets = {
-            {
-              filetype = "NvimTree",
-              text = "File Explorer",
-              highlight = "Directory",
-              text_align = "center",
-            },
-          },
-        },
-      })
-    end,
-  },
+	-- Dashboard --
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			local dashboard = require("alpha.themes.dashboard")
+			local startify = require("alpha.themes.startify")
+			startify.file_icons.provider = "devicons"
 
-  -- Status Line --
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("lualine").setup({
-        options = {
-          disabled_filetypes = { "alpha", "NvimTree" },
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = {
-            { "filetype", icon_only = true,   separator = "", padding = { left = 1, right = 0 } },
-            { "filename", file_status = true, path = 1 },
-            { "branch" },
-            { "diff" },
-          },
-          lualine_c = { "diagnostics" },
-          lualine_x = { "encoding", "fileformat", "rest" },
-          lualine_y = {
-            { "progress", padding = { left = 1, right = 0 }, separator = "" },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-          lualine_z = {
-            function()
-              return " " .. os.date("%R")
-            end,
-          },
-        },
-      })
-    end,
-  },
+			-- Header --
+			dashboard.section.header.val = {
+				[[                                              ]],
+				[[                                              ]],
+				[[█████████╗██╗███████╗██████╗███████╗███╗   ██╗]],
+				[[ ╚══██╔══╝██║██╔════╝██╔═══╝██╔════╝████╗  ██║]],
+				[[    ██║   ██║█████╗  ██████╗█████╗  ██╔██╗ ██║]],
+				[[    ██║   ██║██╔══╝  ╚═══██║██╔══╝  ██║╚██╗██║]],
+				[[    ██║   ██║███████╗██████║███████╗██║ ╚████║]],
+				[[    ╚═╝   ╚═╝╚══════╝╚═════╝╚══════╝╚═╝  ╚═══╝]],
+				[[                                              ]],
+				[[                    @tiesen243                ]],
+			}
 
-  -- Indent Blank Line --
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-      require("ibl").setup()
-    end,
-  },
+			-- Buttons --
+			dashboard.section.buttons.val = {
+				dashboard.button("n", "  New file", "<cmd>enew<CR>"),
+				dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
+				dashboard.button("g", "󰈬  Live grep", "<cmd>Telescope live_grep<CR>"),
+				dashboard.button("r", "  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
+				dashboard.button("c", "  Configuration", "<cmd>e ~/.config/nvim/init.lua<CR>"),
+				dashboard.button("q", "⏻  Quit", "<cmd>wqa<CR>"),
+			}
+			dashboard.section.buttons.opts = {
+				spacing = 0,
+				position = "center",
+			}
 
-  -- Dashboard --
-  {
-    "goolord/alpha-nvim",
-    config = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local startify = require("alpha.themes.startify")
-      startify.file_icons.provider = "devicons"
+			-- Footer --
+			dashboard.section.footer.val = function()
+				local stats = require("lazy").stats()
+				local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
 
-      -- Header --
-      dashboard.section.header.val = {
-        [[                                              ]],
-        [[                                              ]],
-        [[█████████╗██╗███████╗██████╗███████╗███╗   ██╗]],
-        [[ ╚══██╔══╝██║██╔════╝██╔═══╝██╔════╝████╗  ██║]],
-        [[    ██║   ██║█████╗  ██████╗█████╗  ██╔██╗ ██║]],
-        [[    ██║   ██║██╔══╝  ╚═══██║██╔══╝  ██║╚██╗██║]],
-        [[    ██║   ██║███████╗██████║███████╗██║ ╚████║]],
-        [[    ╚═╝   ╚═╝╚══════╝╚═════╝╚══════╝╚═╝  ╚═══╝]],
-        [[                                              ]],
-        [[                    @tiesen243                ]],
-      }
+				return {
+					[[                                              ]],
+					[[                                              ]],
+					[[                  I believe this world is flat]],
+					[[                      Because loli is my world]],
+					[[                                              ]],
+					[[                                              ]],
+					"   ⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms ⚡",
+				}
+			end
 
-      -- Buttons --
-      dashboard.section.buttons.val = {
-        dashboard.button("n", "  New file", "<cmd>enew<CR>"),
-        dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
-        dashboard.button("g", "󰈬  Live grep", "<cmd>Telescope live_grep<CR>"),
-        dashboard.button("r", "  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
-        dashboard.button("c", "  Configuration", "<cmd>e ~/.config/nvim/init.lua<CR>"),
-        dashboard.button("q", "⏻  Quit", "<cmd>wqa<CR>"),
-      }
-      dashboard.section.buttons.opts = {
-        spacing = 0,
-        position = "center",
-      }
+			-- Configuration --
+			dashboard.section.footer.opts.hl = "Type"
+			dashboard.section.header.opts.hl = "Include"
+			dashboard.section.buttons.opts.hl = "Keyword"
+			dashboard.opts.opts.noautocmd = true
 
-      -- Footer --
-      dashboard.section.footer.val = function()
-        local stats = require("lazy").stats()
-        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+			-- Set up
+			require("alpha").setup(dashboard.opts)
+		end,
+	},
 
-        return {
-          [[                                              ]],
-          [[                                              ]],
-          [[                  I believe this world is flat]],
-          [[                      Because loli is my world]],
-          [[                                              ]],
-          [[                                              ]],
-          "   ⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms ⚡",
-        }
-      end
+	-- Noice --
+	{
+		"folke/noice.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = function()
+			require("noice").setup({
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = true, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = true, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	},
 
-      -- Configuration --
-      dashboard.section.footer.opts.hl = "Type"
-      dashboard.section.header.opts.hl = "Include"
-      dashboard.section.buttons.opts.hl = "Keyword"
-      dashboard.opts.opts.noautocmd = true
-
-      -- Set up
-      require("alpha").setup(dashboard.opts)
-    end,
-  },
-
-  -- Noice --
-  {
-    "folke/noice.nvim",
-    config = function()
-      require("noice").setup({
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        presets = {
-          bottom_search = true,    -- use a classic bottom cmdline for search
-          command_palette = true,  -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = true,       -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true,   -- add a border to hover docs and signature help
-        },
-      })
-    end,
-  },
-
-  -- Tmux --
-  {
-    "aserowy/tmux.nvim",
-    lazy = false,
-    keys = {
-      { "<C-Up>",    "<cmd>lua require('tmux').resize_top()<cr>",    desc = "Increase Window Height" },
-      { "<C-Left>",  "<cmd>lua require('tmux').resize_left()<cr>",   desc = "Decrease Window Width" },
-      { "<C-Right>", "<cmd>lua require('tmux').resize_right()<cr>",  desc = "Increase Window Width" },
-      { "<C-Down>",  "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "Decrease Window Height" },
-    },
-    config = function()
-      require("tmux").setup({
-        resize = { enable_default_keybindings = false },
-      })
-    end,
-  },
+	-- Tmux --
+	{
+		"aserowy/tmux.nvim",
+		lazy = false,
+		keys = {
+			{ "<C-Up>", "<cmd>lua require('tmux').resize_top()<cr>", desc = "Increase Window Height" },
+			{ "<C-Left>", "<cmd>lua require('tmux').resize_left()<cr>", desc = "Decrease Window Width" },
+			{ "<C-Right>", "<cmd>lua require('tmux').resize_right()<cr>", desc = "Increase Window Width" },
+			{ "<C-Down>", "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "Decrease Window Height" },
+		},
+		config = function()
+			require("tmux").setup({
+				resize = { enable_default_keybindings = false },
+			})
+		end,
+	},
 }
