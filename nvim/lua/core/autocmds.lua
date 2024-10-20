e01a7f14423df3a5e99a722796e883f95d800b46
tiesen243 <ttien56906@gmail.com>
2024-10-20 13:18:19 +0700

    feat(nvim): map mdx to markdown autocmd

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,63 +1,64 @@
 local function augroup(name)
-  return vim.api.nvim_create_augroup("yuki_" .. name, { clear = true })
+	return vim.api.nvim_create_augroup("yuki_" .. name, { clear = true })
 end
 
 -- Highlight yanked text
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup("highlight_yank"),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- Automatically format on save
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = augroup("format_on_save"),
-  callback = function()
-    vim.lsp.buf.format({ timeout_ms = 2000 })
-  end,
+	group = augroup("format_on_save"),
+	callback = function()
+		vim.lsp.buf.format({ timeout_ms = 2000 })
+	end,
 })
 
 -- Fix conceallevel for json files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = augroup("json_conceal"),
-  pattern = { "json", "jsonc" },
-  callback = function()
-    vim.opt_local.conceallevel = 0
-  end,
+	group = augroup("json_conceal"),
+	pattern = { "json", "jsonc" },
+	callback = function()
+		vim.opt_local.conceallevel = 0
+	end,
 })
 
 -- wrap and check for spell in text filetypes
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("wrap_spell"),
-  pattern = { "*.txt", "*.tex", "*.typ", "gitcommit", "markdown", "mdx" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = augroup("wrap_spell"),
+	pattern = { "*.txt", "*.tex", "*.typ", "gitcommit", "markdown", "mdx" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
 -- map mdx to markdown filetype
-vim.api.nvim_create_autocmd("BufNewFile,BufRead", {
-  group = augroup("mdx_to_markdown"),
-  pattern = { "*.mdx" },
-  callback = function(event)
-    vim.bo[event.buf].filetype = "markdown"
-  end,
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = augroup("mdx_to_markdown"),
+	pattern = { "mdx" },
+	callback = function(event)
+		vim.bo[event.buf].filetype = "markdown"
+	end,
 })
 
 -- close some filetypes with <q>
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("close_with_q"),
-  pattern = {
-    "help",
-    "lspinfo",
-    "startuptime",
-    "checkhealth",
-    "gitsigns.blame",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = augroup("close_with_q"),
+	pattern = {
+		"help",
+		"lspinfo",
+		"startuptime",
+		"checkhealth",
+		"gitsigns.blame",
+		"rest_nvim_result",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
 })
