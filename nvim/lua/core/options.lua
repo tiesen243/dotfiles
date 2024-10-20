5e599fe48b7e3d41a01979dc2fc45e80f4416648
tiesen243 <ttien56906@gmail.com>
2024-10-20 13:09:03 +0700

    feat: map mdx to markdown autocommand

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -40,7 +40,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = false
 vim.opt.guicursor =
-	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
@@ -65,4 +65,4 @@ vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
-vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.ignorecase = true     -- Case insensitive searching UNLESS /C or capital in search
