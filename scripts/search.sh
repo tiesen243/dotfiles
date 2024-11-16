3e8b1677cfba30a710090f102b568d638fcddc06
tiesen243 <ttien56906@gmail.com>
2024-11-16 08:25:23 +0700

    feat(search): move to workspace after search

--- scripts/search.sh
+++ scripts/search.sh
@@ -11,8 +11,14 @@ if [ -z "$query" ]; then
   exit 0
 fi
 
+# check if query is a URL
 if [[ "$query" =~ ^(http://|https://)?([a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(:[0-9]+)?(/.*)?$ ]]; then
   $1 $query
 else
   $1 "https://www.google.com/search?q=$query"
 fi
+
+# move window to workspace of browser
+window_info=$(hyprctl clients | grep -B 5 "zen-alpha")
+workspace="$(echo "$window_info" | grep "workspace" | head -n 1 | awk '{print $2}')"
+hyprctl dispatch workspace $workspace
