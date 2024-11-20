e835aa69dfb2e92aa14d3fdfe05b1976ce59f435
tiesen243 <ttien56906@gmail.com>
2024-11-20 15:06:46 +0700

    feat: add new plugins and configure existing ones
    - Added 'projekt0n/github-nvim-theme' for GitHub theme support.
    - Configured 'xiyaowong/transparent.nvim' for transparency settings.
    - Integrated 'nvim-tree/nvim-tree.lua' for file explorer functionality.
    - Set up 'nvim-lualine/lualine.nvim' for statusline customization.
    - Added 'folke/which-key.nvim' for key binding assistance.
    - Configured 'nvim-telescope/telescope.nvim' for fuzzy finding.
    - Integrated 'aserowy/tmux.nvim' for tmux support.
    - Added 'lewis6991/gitsigns.nvim' for Git signs.
    - Configured 'folke/snacks.nvim' for UI components.

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,4 +1,20 @@
 return {
+  -- Github theme
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    opts = { transparent = vim.g.transparent_enabled },
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    opts = function()
+      require("transparent").clear_prefix("NvimTree")
+      return { exclude_groups = { "CursorLine" } }
+    end,
+  },
+
   -- file explorer
   {
     "nvim-tree/nvim-tree.lua",
@@ -27,6 +43,40 @@ return {
         },
       }
     end,
+  },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "auto",
+        disabled_filetypes = { statusline = { "snacks_dashboard", "NvimTree" } },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", file_status = true, path = 1 },
+          { "branch" },
+          { "diff" },
+        },
+        lualine_c = { "diagnostics" },
+        lualine_x = { "encoding", "fileformat", "rest" },
+        lualine_y = {
+          { "progress", padding = { left = 1, right = 0 }, separator = " " },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      },
+    },
   },
 
   -- which-key helps you remember key bindings by showing a popup
@@ -125,15 +175,38 @@ return {
     },
   },
 
-  -- colorize hex colors in the buffer
+  -- ui components
   {
-    "NvChad/nvim-colorizer.lua",
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
     opts = {
-      --- @type string | "foreground" | "background" | "virtualtext"
-      mode = "background", -- Set the display mode.
-      virtualtext = "■",
-      --- @type boolean | "normal" | "lsp" | "both"
-      tailwind = "lsp", -- Enable tailwind colors
+      dashboard = {
+        preset = {
+          header = [[
+██╗   ██╗██╗   ██╗██╗  ██╗██╗
+╚██╗ ██╔╝██║   ██║██║ ██╔╝██║
+ ╚████╔╝ ██║   ██║█████╔╝ ██║
+  ╚██╔╝  ██║   ██║██╔═██╗ ██║
+   ██║   ╚██████╔╝██║  ██╗██║
+   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝
+          ]],
+        },
+
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+          { section = "keys", gap = 1, padding = 1, pane = 2 },
+        },
+      },
+
+      words = { enabled = true },
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      notifier = { enabled = true, timeout = 3000 },
+      styles = { notification = { wo = { wrap = true } } },
     },
   },
 }
