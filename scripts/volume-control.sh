e7ea09b0df0a524f8b695760fd25dd9b6c60bd9a
tiesen243 <ttien56906@gmail.com>
2024-12-08 17:41:20 +0700

    chore: update scripts

--- scripts/volume-control.sh
+++ scripts/volume-control.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-title="Volume"
 currentVolume="$(pactl list sinks | grep 'Volume:' | head -n 1 | awk '{print $5}')"
 isMuted="$(pactl list sinks | grep 'Mute:' | head -n 1 | awk '{print $2 == "yes" ? "Muted" : "Unmuted"}')"
 
 if [ $1 = "up" ]; then
   pactl set-sink-volume @DEFAULT_SINK@ +5% 
-  notify-send $title "Increased to $currentVolume"
+  notify-send "Volume increased to $currentVolume"
 elif [ $1 = "down" ]; then
   pactl set-sink-volume @DEFAULT_SINK@ -5%
-  notify-send $title "Decreased to $currentVolume"
+  notify-send "Volume decreased to $currentVolume"
 elif [ $1 = "toggle" ]; then
   pactl set-sink-mute @DEFAULT_SINK@ toggle
-  notify-send "$title" "$isMuted"
+  notify-send "Volume $isMuted"
 fi
