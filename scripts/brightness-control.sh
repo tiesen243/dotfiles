13bdc7eef04028126c738ae38d7a8fabcc299548
tiesen243 <ttien56906@gmail.com>
2024-12-08 17:41:20 +0700

    chore: update scripts

--- scripts/brightness-control.sh
+++ scripts/brightness-control.sh
@@ -4,10 +4,10 @@ screenBrightness="$(brightnessctl g | awk '{print int($1 / 19200 * 100)}')%"
 
 if [ $1 = "up" ]; then
   brightnessctl s +5%
-  notify-send "Brightness" "Increased to $screenBrightness"
+  notify-send "Increased to $screenBrightness"
 elif [ $1 = "down" ]; then
   brightnessctl s 5%-
-  notify-send "Brightness" "Decreased to $screenBrightness"
+  notify-send "Decreased to $screenBrightness"
 fi
 
 
