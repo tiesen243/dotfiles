e271aaa7c1cfa5b2d67c4277a2607795598b36c9
tiesen243 <ttien56906@gmail.com>
2024-12-30 05:35:23 +0700

    feat(nvim): update keymap

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,23 +1,78 @@
 local opt = vim.opt
 
 -- General
-opt.syntax = "on"
-opt.foldlevel = 99
-vim.wo.foldmethod = "indent"
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.autowrite = true                                    -- Automatically save before running commands
+opt.autoread = true                                     -- Automatically read file if changed outside of vim
 opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
-opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 2                                    -- Hide * markup for bold and italic, but not markers with substitutions
+opt.confirm = true                                      -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true                                   -- Enable highlighting of the current line
+opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
+opt.foldlevel = 99
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+opt.foldmethod = "expr"
+opt.foldtext = ""
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true      -- Ignore case
+opt.inccommand = "nosplit" -- preview incremental substitute
+opt.jumpoptions = "view"
+opt.laststatus = 3         -- global statusline
+opt.linebreak = true       -- Wrap lines at convenient points
+opt.list = true            -- Show some invisible characters (tabs...
+opt.mouse = "a"            -- Enable mouse mode
+opt.number = true          -- Print line number
+opt.pumblend = 10          -- Popup blend
+opt.pumheight = 10         -- Maximum number of entries in a popup
+opt.relativenumber = false -- Relative line numbers
+opt.ruler = false          -- Disable the default ruler
+opt.scrolloff = 4          -- Lines of context
+opt.sessionoptions =
+{ "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.showmode = false   -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8  -- Columns of context
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true   -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
+opt.spelllang = { "en" }
+opt.splitbelow = true  -- Put new windows below current
+opt.splitkeep = "screen"
+opt.splitright = true  -- Put new windows right of current
+opt.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]]
+opt.syntax = "on"
+opt.smoothscroll = true
+opt.termguicolors = true                      -- True color support
+opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
+opt.undofile = true
+opt.undolevels = 10000
+opt.updatetime = 200               -- Save swap file and trigger CursorHold
+opt.virtualedit = "block"          -- Allow cursor to move where there is no text in visual block mode
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.winminwidth = 5                -- Minimum window width
+opt.wrap = false                   -- Disable line wrap
+
+-- Snacks animations
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = true
 
 -- Tab and shift
-opt.tabstop = 2 -- Number of spaces tabs count for
-opt.expandtab = true -- Use spaces instead of tabs
+opt.tabstop = 2       -- Number of spaces tabs count for
+opt.expandtab = true  -- Use spaces instead of tabs
 opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
+opt.shiftwidth = 2    -- Size of an indent
 
--- Disable swapfile
-opt.swapfile = false
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
@@ -30,3 +85,6 @@ vim.filetype.add({
     conf = "bash",
   },
 })
+
+-- Disable swapfile
+opt.swapfile = false
