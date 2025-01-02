67d9ad18dffeca99fb79ae07199e308d6c6be48e
tiesen243 <ttien56906@gmail.com>
2025-01-02 12:07:37 +0700

    feat(nvim): choose venv in python

--- nvim/lua/plugins/transparent.lua
+++ nvim/lua/plugins/transparent.lua
@@ -4,6 +4,8 @@ return {
   init = function()
     require("transparent").clear_prefix("NeoTree")
     require("transparent").clear_prefix("WhichKey")
+    require("transparent").clear_prefix("Telescope")
+    require("transparent").clear_prefix("snack")
   end,
   opts = {
     extra_groups = { "NormalFloat" },
