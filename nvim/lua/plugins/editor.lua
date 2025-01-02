62d19f9166f5b467c3da74c35cab6e433dc0d222
tiesen243 <ttien56906@gmail.com>
2025-01-02 11:41:28 +0700

    style(nvim): change logo

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,4 +3,23 @@ return {
     "folke/which-key.nvim",
     opts = { preset = "moderm", win = { border = "rounded" } },
   },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+██╗   ██╗██╗   ██╗██╗  ██╗██╗
+╚██╗ ██╔╝██║   ██║██║ ██╔╝██║
+ ╚████╔╝ ██║   ██║█████╔╝ ██║
+  ╚██╔╝  ██║   ██║██╔═██╗ ██║
+   ██║   ╚██████╔╝██║  ██╗██║
+   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝
+          ]],
+        },
+      },
+    },
+  },
 }
