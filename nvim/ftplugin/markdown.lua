39444ce1c43aa87bd0006ee14887c91efbe08e37
tiesen243 <ttien56906@gmail.com>
2025-01-09 19:32:44 +0700

    feat(nvim): add jdtls keymaps

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -4,9 +4,9 @@ vim.b.breakindent = true -- Match indent on line break
 vim.b.linebreak = true   -- Line break on whole words
 
 -- Allow j/k when navigating wrapped lines
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
 
 -- Spell check
-vim.b.spelllang = "en_us"
+vim.b.spelllang = 'en_us'
 vim.b.spell = true
