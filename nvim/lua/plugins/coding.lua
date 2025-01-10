c048a95ce5dcdaf49e0196713b3d7063998eb9bd
tiesen243 <ttien56906@gmail.com>
2025-01-10 21:54:36 +0700

    feat(nvim): change completion to blink

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,101 +1,59 @@
--- Register filetype and extension for treesitter
-vim.filetype.add {
-  pattern = { ['.*/hypr/.*%.conf'] = 'hyprlang' },
-  extension = {
-    mdx = 'markdown',
-  },
-}
-vim.treesitter.language.register('markdown', 'mdx')
-
 -- Format on save
 vim.api.nvim_create_autocmd('BufWritePre', {
   group = vim.api.nvim_create_augroup('LspFormatting', {}),
   callback = function()
-    vim.lsp.buf.format { timeout = 2000 }
+    pcall(vim.lsp.buf.format, { timeout = 2000 })
+  end,
+})
+
+-- Hide copilot suggestions when cmp menu is open
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuOpen',
+  callback = function()
+    vim.b.copilot_enabled = false
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuClose',
+  callback = function()
+    vim.b.copilot_enabled = true
   end,
 })
 
 return {
   -- Code Completion
-  -- https://github.com/hrsh7th/nvim-cmp
+  -- https://github.com/saghen/blink.cmp
   {
-    'hrsh7th/nvim-cmp',
+    'saghen/blink.cmp',
+    version = '*',
     event = 'InsertEnter',
-    dependencies = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path' },
-    opts = function()
-      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
-      local defaults = require 'cmp.config.default' ()
-      local cmp = require 'cmp'
-
-      cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
-
-      return {
-        completion = { competeopt = 'menu,menuone,preview,noselect' },
-        preselect = cmp.PreselectMode.Item,
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<S-Tab>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Replace },
-          ['<Tab>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Replace },
-          ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-j>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm { select = false },
-        },
-        sources = cmp.config.sources {
-          { name = 'nvim_lsp' },
-          { name = 'snippets' },
-          { name = 'buffer' },
-          { name = 'path' },
-        },
-        formatting = {
-          fields = { 'kind', 'abbr', 'menu' },
-          format = function(entry, item)
-            if vim.tbl_contains({ 'path' }, entry.source.name) then
-              local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-              if icon then
-                item.kind = icon
-                item.kind_hl_group = hl_group
-                return item
-              end
-            end
-
-            item.kind = Yuki.icons.kinds[item.kind] or ''
-
-            local widths = {
-              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 30,
-              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 20,
-            }
-
-            for key, width in pairs(widths) do
-              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. '…'
-              end
-            end
-
-            return item
-          end,
-        },
-        experimental = { ghost_text = { hl_group = 'CmpGhostText' } },
-        sorting = defaults.sorting,
-      }
-    end,
-  },
-
-  -- Snippets engine
-  -- https://github.com/garymjr/nvim-snippets
-  {
-    'garymjr/nvim-snippets',
-    dependencies = 'rafamadriz/friendly-snippets',
-    opts = { friendly_snippets = true },
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    opts = {
+      appearance = {
+        nerd_font_variant = 'mono',
+        use_nvim_cmp_as_default = false,
+        kind_icons = vim.tbl_extend('keep', { Color = '██' }, Yuki.icons.kinds),
+      },
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+        list = { selection = { preselect = false, auto_insert = true } },
+        menu = { draw = { treesitter = { 'lsp' } } },
+        documentation = { auto_show = true, auto_show_delay_ms = 300 },
+        ghost_text = { enabled = true },
+      },
+      keymap = {
+        preset = 'none',
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<C-Space>'] = { 'show', 'hide', 'fallback' },
+        ['<C-k>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-j>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+      sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+    },
+    opts_extend = { 'sources.default' },
   },
 
   -- Formatter
@@ -115,29 +73,6 @@ return {
     end,
   },
 
-  -- Treesitter
-  -- https://github.com/nvim-treesitter/nvim-treesitter
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    event = 'VeryLazy',
-    main = 'nvim-treesitter.configs',
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    opts = {
-      auto_install = true,
-      sync_install = true,
-      indent = { enable = true },
-      highlight = { enable = true, additional_vim_regex_highlighting = true },
-    },
-  },
-
-  -- Automatically add closing tags for HTML and JSX
-  -- https://github.com/windwp/nvim-ts-autotag
-  {
-    'windwp/nvim-ts-autotag',
-    opts = {},
-  },
-
   -- Auto pairs
   -- https://github.com/windwp/nvim-autopairs
   {
@@ -147,14 +82,10 @@ return {
   },
 
   -- Comment toggler
-  -- https://github.com/numToStr/Comment.nvim
+  -- https://github.com/folke/ts-comments.nvim
   {
-    'numToStr/Comment.nvim',
-    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
-    opts = {
-      pre_hook = function()
-        return vim.bo.commentstring
-      end,
-    },
+    'folke/ts-comments.nvim',
+    event = 'VeryLazy',
+    opts = {},
   },
 }
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+-- Register filetype and extension for treesitter
+vim.filetype.add {
+  extension = { rasi = 'rasi', rofi = 'rasi', wofi = 'rasi', mdx = 'markdown' },
+  pattern = {
+    ['*/waybar/config'] = 'jsonc',
+    ['*/kitty/.+%.conf'] = 'kitty',
+    ['*/hypr/.+%.conf'] = 'hyprlang',
+  },
+}
+vim.treesitter.language.register('bash', 'kitty')
+vim.treesitter.language.register('markdown', 'mdx')
+
+return {
+  -- Treesitter
+  -- https://github.com/nvim-treesitter/nvim-treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = 'VeryLazy',
+    main = 'nvim-treesitter.configs',
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    opts = {
+      auto_install = true,
+      sync_install = true,
+      indent = { enable = true },
+      highlight = { enable = true, additional_vim_regex_highlighting = true },
+    },
+  },
+
+  -- Automatically add closing tags for HTML and JSX
+  -- https://github.com/windwp/nvim-ts-autotag
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {},
+  },
+}
