01473378878c9d5fc34e613a517e52602e90f8f3
tiesen243 <ttien56906@gmail.com>
2025-01-14 23:31:43 +0700

    feat: switch blink to cmp

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -6,54 +6,109 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
--- Hide copilot suggestions when cmp menu is open
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'BlinkCmpMenuOpen',
-  callback = function()
-    vim.b.copilot_enabled = false
-  end,
-})
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'BlinkCmpMenuClose',
-  callback = function()
-    vim.b.copilot_enabled = true
-  end,
-})
-
 return {
   -- Code Completion
-  -- https://github.com/saghen/blink.cmp
+  -- https://github.com/hrsh7th/nvim-cmp
   {
-    'saghen/blink.cmp',
-    version = '*',
-    event = 'InsertEnter',
+    'hrsh7th/nvim-cmp',
+    event = { 'InsertEnter' },
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path' },
+    opts = function()
+      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
+      local cmp = require 'cmp'
+      local defaults = require 'cmp.config.default' ()
+
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+      return {
+        completion = { completeopt = 'menu,menuone,noinsert,noselect' },
+        preselect = cmp.PreselectMode.Item,
+        snippet = {
+          expand = function(args)
+            return vim.snippet.expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-j>'] = cmp.mapping.scroll_docs(4),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
+            else
+              fallback()
+            end
+          end, { 'i' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item { behavior = cmp.SelectBehavior.Insert }
+            else
+              fallback()
+            end
+          end, { 'i' }),
+          ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm { select = true }
+            else
+              fallback()
+            end
+          end, { 'i' }),
+          ['<C-Space>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.abort()
+            else
+              cmp.complete()
+            end
+          end, { 'i' }),
+        },
+        sources = cmp.config.sources {
+          { name = 'nvim_lsp' },
+          { name = 'path' },
+          { name = 'buffer' },
+          { name = 'snippets' },
+        },
+        formatting = {
+          fields = { 'kind', 'abbr', 'menu' },
+          format = function(entry, item)
+            item.kind = (Yuki.icons.kinds[item.kind] or '')
+
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 30,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 20,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. '…'
+              end
+            end
+
+            if vim.tbl_contains({ 'path' }, entry.source.name) then
+              local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+              if icon then
+                item.kind = icon
+                item.kind_hl_group = hl_group
+                return item
+              end
+            end
+
+            return item
+          end,
+        },
+        experimental = { ghost_text = { hl_group = 'CmpGhostText' } },
+        sorting = defaults.sorting,
+      }
+    end,
+  },
+
+  -- Snippets
+  {
+    'garymjr/nvim-snippets',
     dependencies = { 'rafamadriz/friendly-snippets' },
     opts = {
-      appearance = {
-        nerd_font_variant = 'mono',
-        use_nvim_cmp_as_default = false,
-        kind_icons = vim.tbl_extend('keep', { Color = '██' }, Yuki.icons.kinds),
-      },
-      completion = {
-        accept = { auto_brackets = { enabled = true } },
-        list = { selection = { preselect = false, auto_insert = true } },
-        menu = { draw = { treesitter = { 'lsp' } } },
-        documentation = { auto_show = true, auto_show_delay_ms = 300 },
-        ghost_text = { enabled = true },
-      },
-      keymap = {
-        preset = 'none',
-        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
-        ['<CR>'] = { 'accept', 'fallback' },
-        ['<C-Space>'] = { 'show', 'hide', 'fallback' },
-        ['<C-k>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-j>'] = { 'scroll_documentation_down', 'fallback' },
-      },
-      sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+      create_cmp_source = true,
+      friendly_snippets = true,
     },
-    opts_extend = { 'sources.default' },
   },
 
   -- Formatter
