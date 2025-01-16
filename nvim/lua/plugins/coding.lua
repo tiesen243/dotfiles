09557aa09f0ad80daa8cd19f1643bf4c738db1b7
tiesen243 <ttien56906@gmail.com>
2025-01-16 12:56:55 +0700

    feat(nvim): switch to blink

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -6,119 +6,45 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
+-- Disable copilot when completion menu is open
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuOpen',
+  callback = function()
+    vim.b.copilot_enabled = false
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuClose',
+  callback = function()
+    vim.b.copilot_enabled = true
+  end,
+})
+
 return {
   -- Code Completion
-  -- https://github.com/hrsh7th/nvim-cmp
+  -- https://github.com/saghen/blink.cmp
   {
-    'hrsh7th/nvim-cmp',
-    event = { 'InsertEnter' },
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'saadparwaiz1/cmp_luasnip' },
-    opts = function()
-      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local defaults = require 'cmp.config.default' ()
-
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-      return {
-        completion = { completeopt = 'menu,menuone,noinsert,noselect' },
-        preselect = cmp.PreselectMode.Item,
-        snippet = {
-          expand = function(args)
-            return luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-j>'] = cmp.mapping.scroll_docs(4),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.locally_jumpable(1) then
-              luasnip.jump(1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<CR>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              if luasnip.expandable() then
-                luasnip.expand()
-              else
-                cmp.confirm { select = true }
-              end
-            else
-              fallback()
-            end
-          end),
-          ['<C-Space>'] = cmp.mapping(function()
-            if cmp.visible() then
-              cmp.abort()
-            else
-              cmp.complete()
-            end
-          end, { 'i' }),
-        },
-        sources = cmp.config.sources {
-          { name = 'nvim_lsp' },
-          { name = 'path' },
-          { name = 'buffer' },
-          { name = 'luasnip' },
-        },
-        formatting = {
-          fields = { 'kind', 'abbr', 'menu' },
-          format = function(entry, item)
-            item.kind = (Yuki.icons.kinds[item.kind] or '')
-
-            local widths = {
-              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 30,
-              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 20,
-            }
-
-            for key, width in pairs(widths) do
-              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. '…'
-              end
-            end
-
-            if vim.tbl_contains({ 'path' }, entry.source.name) then
-              local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-              if icon then
-                item.kind = icon
-                item.kind_hl_group = hl_group
-                return item
-              end
-            end
-
-            return item
-          end,
-        },
-        experimental = { ghost_text = { hl_group = 'CmpGhostText' } },
-        sorting = defaults.sorting,
-      }
-    end,
-  },
-
-  -- Snippets
-  -- https://github.com/L3MON4D3/LuaSnip
-  {
-    'L3MON4D3/LuaSnip',
+    'saghen/blink.cmp',
+    version = '*',
     dependencies = { 'rafamadriz/friendly-snippets' },
-    opts = function()
-      require('luasnip.loaders.from_vscode').lazy_load()
-      return {}
-    end,
+    event = 'InsertEnter',
+    opts = {
+      keymap = { preset = 'enter', ['<C-y>'] = { 'select_and_accept' } },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono',
+        kind_icons = vim.tbl_extend('keep', { Color = '██' }, Yuki.icons.kinds),
+      },
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+        menu = { draw = { treesitter = { 'lsp' } } },
+        documentation = { auto_show = true, auto_show_delay_ms = 200 },
+        ghost_text = { enabled = true },
+      },
+      sources = { default = { 'lsp', 'path', 'buffer', 'snippets' } },
+    },
+    opts_extend = { 'sources.default' },
   },
 
   -- Formatter
