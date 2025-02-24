6026c1fd9d5ba003e78b90c3ca2b0060dd707fef
tiesen243 <ttien56906@gmail.com>
2025-02-24 19:23:39 +0700

    chore(hypr): change opacity

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -25,7 +25,7 @@ return {
         menu = { draw = { treesitter = { "lsp" } } },
         documentation = { auto_show = true, auto_show_delay_ms = 200 },
         ghost_text = { enabled = true },
-        list = { selection = { preselect = false, auto_insert = true } },
+        list = { selection = { preselect = true, auto_insert = true } },
       },
 
       sources = {
