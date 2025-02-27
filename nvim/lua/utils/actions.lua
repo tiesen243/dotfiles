55c355f110fc13ff141e1d8e1b0c78c4393fab06
tiesen243 <ttien56906@gmail.com>
2025-02-27 16:03:44 +0700

    feat(nvim): search google with <leader>fG

--- nvim/lua/utils/actions.lua
+++ nvim/lua/utils/actions.lua
@@ -66,4 +66,19 @@ M.get_time = function()
   return " " .. os.date("%R")
 end
 
+M.google_search = function()
+  vim.ui.input({ prompt = "Search Google: " }, function(input)
+    if input == nil or input == "" then -- Esc or nil to quit google_search
+      vim.api.nvim_input("<Esc>")
+      return
+    end
+    if input and input ~= "" then
+      local encoded_query = input:gsub(" ", "+") -- Replace spacebar to "+"
+      local url = "https://www.google.com/search?q=" .. encoded_query
+      local open_cmd = "xdg-open" -- Use "open" for macOS, "start" for Windows
+      os.execute(open_cmd .. " '" .. url .. "'")
+    end
+  end)
+end
+
 return M
