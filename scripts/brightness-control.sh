3f607ff597daccd62669db5f5ed79270b719c361
tiesen243 <ttien56906@gmail.com>
2025-02-28 12:44:57 +0700

    feat(hypr): change some key binds

--- scripts/brightness-control.sh
+++ scripts/brightness-control.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 getCurrentBrightness() {
-	current=$(cat /sys/class/backlight/*/brightness)
-	max=$(cat /sys/class/backlight/*/max_brightness)
-	percentage=$(((current * 100) / max))
-	echo "$percentage%"
+  current=$(cat /sys/class/backlight/*/brightness)
+  max=$(cat /sys/class/backlight/*/max_brightness)
+  percentage=$(((current * 100) / max))
+  echo "$percentage%"
 }
 
 if [ $1 = "--up" ]; then
-	dunstctl close-all
-	brightnessctl s +5%
-	notify-send "Brightness increased to $(getCurrentBrightness)"
+  dunstctl close-all
+  brightnessctl s +5%
+  notify-send "Brightness increased to $(getCurrentBrightness)"
 elif [ $1 = "--down" ]; then
-	dunstctl close-all
-	brightnessctl s 5%-
-	notify-send "Brightness decreased to $(getCurrentBrightness)"
+  dunstctl close-all
+  brightnessctl s 5%-
+  notify-send "Brightness decreased to $(getCurrentBrightness)"
 fi
