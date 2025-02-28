d530f23c796fff8932974158e8109271d508f406
tiesen243 <ttien56906@gmail.com>
2025-02-28 12:44:57 +0700

    feat(hypr): change some key binds

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 if [ -d ~/OneDrive ]; then
-	saveLocation=~/OneDrive/Pictures/Screenshots
+  saveLocation=~/OneDrive/Pictures/Screenshots
 else
-	saveLocation=~/Pictures/Screenshots
-	if [ ! -d $saveLocation ]; then
-		mkdir -p $saveLocation
-	fi
+  saveLocation=~/Pictures/Screenshots
+  if [ ! -d $saveLocation ]; then
+    mkdir -p $saveLocation
+  fi
 fi
 
 saveLocation=($saveLocation/Screenshot\ $(date +%Y-%m-%d)\ $(date +%H%M%S)\.png)
 
 if [ "$1" == "--full" ]; then
-	grim "$saveLocation"
+  grim "$saveLocation"
 elif [ "$1" == "--select" ]; then
-	grim -g "$(slurp)" "$saveLocation"
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
+  grim -g "$(slurp)" "$saveLocation"
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
 else
-	notify-send "Invalid argument. Use --full or --select."
-	exit 1
+  notify-send "Invalid argument. Use --full or --select."
+  exit 1
 fi
 
 wl-copy <"$saveLocation"
--- scripts/volume-control.sh
+++ scripts/volume-control.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
 getCurrentVolume() {
-	volume=$(pactl list sinks | grep 'Volume:' | head -n 1 | awk '{print $5}')
-	echo "$volume"
+  volume=$(pactl list sinks | grep 'Volume:' | head -n 1 | awk '{print $5}')
+  echo "$volume"
 }
 
 getVolumeStatus() {
-	status=$(pactl list sinks | grep 'Mute:' | head -n 1 | awk '{print $2}')
-	if [ "$status" = "yes" ]; then
-		echo "muted"
-	else
-		echo "unmuted"
-	fi
+  status=$(pactl list sinks | grep 'Mute:' | head -n 1 | awk '{print $2}')
+  if [ "$status" = "yes" ]; then
+    echo "muted"
+  else
+    echo "unmuted"
+  fi
+}
+
+getMicStatus() {
+  status=$(pactl list sources | grep 'Mute:' | head -n 1 | awk '{print $2}')
+  if [ "$status" = "yes" ]; then
+    echo "muted"
+  else
+    echo "unmuted"
+  fi
 }
 
 if [ $1 = "--up" ]; then
-	dunstctl close-all
-	pactl set-sink-volume @DEFAULT_SINK@ +5%
-	notify-send "Volume increased to $(getCurrentVolume)"
+  dunstctl close-all
+  pactl set-sink-volume @DEFAULT_SINK@ +5%
+  notify-send "Volume increased to $(getCurrentVolume)"
 elif [ $1 = "--down" ]; then
-	dunstctl close-all
-	pactl set-sink-volume @DEFAULT_SINK@ -5%
-	notify-send "Volume decreased to $(getCurrentVolume)"
-elif [ $1 = "--toggle" ]; then
-	dunstctl close-all
-	pactl set-sink-mute @DEFAULT_SINK@ toggle
-	notify-send "Volume $(getVolumeStatus)"
+  dunstctl close-all
+  pactl set-sink-volume @DEFAULT_SINK@ -5%
+  notify-send "Volume decreased to $(getCurrentVolume)"
+elif [ $1 = "--toggle-volume" ]; then
+  dunstctl close-all
+  pactl set-sink-mute @DEFAULT_SINK@ toggle
+  notify-send "Volume $(getVolumeStatus)"
 fi
