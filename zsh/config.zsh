98593708a272510a697fce39cb3836d2960fc095
tiesen243 <ttien56906@gmail.com>
2025-04-22 20:16:10 +0700

    chore: fix uv

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -15,7 +15,3 @@ source $HOME/dotfiles/zsh/scripts.zsh
 
 # ------ Export, Env Variable ------
 source $HOME/dotfiles/zsh/exports.zsh
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
