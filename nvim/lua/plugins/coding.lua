8ec2e7efbe5e3393a9ee60428f3b8f45323f8dcf
tiesen243 <ttien56906@gmail.com>
2025-05-16 03:06:44 +0700

    feat(nvim): add hi

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -20,12 +20,22 @@ return {
                   if vim.tbl_contains({ "Path" }, ctx.source_name) then
                     local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
                     if dev_icon then
-                      icon = dev_icon
+                      icon = dev_icon .. " "
                     end
                   else
-                    icon = Yuki.icons.kinds[ctx.kind]
+                    icon = Yuki.icons.kinds[ctx.kind] or " "
                   end
                   return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      hl = dev_hl
+                    end
+                  end
+                  return hl
                 end,
               },
             },
