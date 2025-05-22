56d382c241278a29fdb8badd3e13a77d2256c157
tiesen243 <ttien56906@gmail.com>
2025-05-23 02:47:27 +0700

    feat(nvim): change model to sonnet 4

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -107,6 +107,17 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  desc = "Disable line numbers in CopilotChat",
+  group = Yuki.utils.create_augroup("copilot_chat"),
+  pattern = "copilot-*",
+  callback = function()
+    -- Set buffer-local options
+    vim.opt_local.relativenumber = false
+    vim.opt_local.number = false
+    vim.opt_local.conceallevel = 0
+  end,
+})
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   desc = "Auto create dir when saving a file, in case some intermediate directory does not exist",
   group = Yuki.utils.create_augroup("auto_create_dir"),
--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,5 +1,3 @@
-local ai_cmp = false
-
 return {
   {
     "zbirenbaum/copilot.lua",
@@ -8,36 +6,18 @@ return {
     event = "BufReadPost",
     opts = {
       suggestion = {
-        enabled = not ai_cmp,
+        enabled = true,
         auto_trigger = true,
         hide_during_completion = true,
-        keymap = { accept = not ai_cmp and "<C-y>" or false },
+        keymap = { accept = "<C-y>" },
       },
       panel = { enabled = false },
     },
   },
 
   {
-    "saghen/blink.cmp",
-    optional = true,
-    dependencies = { "giuxtaposition/blink-cmp-copilot", enabled = ai_cmp },
-    opts = {
-      sources = ai_cmp and {
-        default = { "copilot" },
-        providers = {
-          copilot = {
-            name = "copilot",
-            module = "blink-cmp-copilot",
-            score_offset = 100,
-            async = true,
-          },
-        },
-      } or nil,
-    },
-  },
-
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
     cmd = "CopilotChat",
     keys = {
       { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
@@ -82,28 +62,18 @@ return {
     opts = function()
       local user = vim.env.USER or "User"
       user = user:sub(1, 1):upper() .. user:sub(2)
+
       return {
         auto_insert_mode = true,
-        model = "claude-3.7-sonnet-thought",
+        model = "claude-sonnet-4",
         question_header = "  " .. user .. " ",
         answer_header = "  Copilot ",
+        sticky = { "#files" },
         window = {
           width = 0.4,
         },
+        mappings = { reset = { normal = "" } },
       }
-    end,
-    config = function(_, opts)
-      local chat = require("CopilotChat")
-
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "copilot-chat",
-        callback = function()
-          vim.opt_local.relativenumber = false
-          vim.opt_local.number = false
-        end,
-      })
-
-      chat.setup(opts)
     end,
   },
 }
