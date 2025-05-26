3692b105d76862dee47f88d4503aa1f458eafa60
tiesen243 <ttien56906@gmail.com>
2025-05-26 11:03:56 +0700

    feat(git): add git config

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -6,32 +6,6 @@ jc() {
   fi
 }
 
-vf() {
-  local file
-  file=$(fzf \
-    --preview 'bat --style=numbers --color=always --line-range :500 {}' \
-    --preview-window=right:60% \
-    --border \
-    --height=80%)
-  if [ -n "$file" ]; then
-    nvim "$file"
-  fi
-}
-
-vh() {
-  local file
-  file=$(find $HOME -type f \
-    -not -path '*/\.*' -o -path '*/.config/*' |
-    fzf \
-      --preview 'bat --style=numbers --color=always --line-range :500 {}' \
-      --preview-window=right:60% \
-      --border \
-      --height=80%)
-  if [ -n "$file" ]; then
-    nvim "$file"
-  fi
-}
-
 kill-port() {
   local port=$1
   if [ -z "$port" ]; then
@@ -39,7 +13,7 @@ kill-port() {
     return 1
   fi
 
-  local pid=$(ss -tulpn | grep ":$port" | awk '{print $7}' | cut -d'=' -f2 | cut -d',' -f1)
+  local pid=$(ss -tulpn | grep ":3000" | awk '{print $8}' | cut -d',' -f2 | cut -d'=' -f2)
 
   if [ -n "$pid" ]; then
     kill -9 $pid
