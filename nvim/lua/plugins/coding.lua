44a02e59445e9282d5863aea9e47a04fd676cb7c
tiesen243 <ttien56906@gmail.com>
2025-07-09 17:03:57 +0700

    chore: minor changes

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,7 +1,7 @@
 return {
   {
     "saghen/blink.cmp",
-    build = "cargo build --release",
+    version = "1.*",
     dependencies = { "rafamadriz/friendly-snippets" },
     event = "InsertEnter",
     opts = {
