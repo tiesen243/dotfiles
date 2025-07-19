d375d55a2a152bd5ce056f55c815229b8f1ce3ab
tiesen243 <ttien56906@gmail.com>
2025-07-19 21:18:59 +0700

    chore: minor changes

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,7 +3,7 @@ export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
 
 ZSH_THEME="yuki"
 
-plugins=(bun gitignore sudo vi-mode z zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(sudo vi-mode z zsh-autosuggestions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
