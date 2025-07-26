38f45b6c61c037da4e558911fab79aca97834b7c
tiesen243 <ttien56906@gmail.com>
2025-07-27 03:45:50 +0700

    chore: minor changes

--- nvim/lua/yuki/init.lua
+++ nvim/lua/yuki/init.lua
@@ -3,8 +3,14 @@ local M = {}
 M.utils = require("yuki.utils")
 M.format = require("yuki.format")
 
-M.configs = {}
-M.configs.icons = require("yuki.icons")
+M.configs = {
+  icons = require("yuki.icons"),
+}
+
+---@param options {colorscheme: string, logo: string}
+M.merge_config = function(options)
+  M.configs = vim.tbl_deep_extend("force", M.configs, options or {})
+end
 
 M.setup = function()
   -- Load core configuration modules
