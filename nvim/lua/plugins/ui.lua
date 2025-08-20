57e691a876dcd67e6e4a519d6457c9a14f8a0069
tiesen243 <ttien56906@gmail.com>
2025-08-21 00:02:42 +0700

    feat: done

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     -- dir = "~/Documents/vercel.nvim",
-    opts = { theme = "dark", transparent = false },
+    opts = { theme = "dark", transparent = true },
   },
 
   {
