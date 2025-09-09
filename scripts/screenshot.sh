6d9a18fe2ecd4e38449a7ac7b27cfe35bad5ce97
tiesen243 <ttien56906@gmail.com>
2025-09-10 06:28:08 +0700

    feat: update screenshot script

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,36 +1,80 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ -d ~/OneDrive ]; then
-  saveLocation=~/OneDrive/Pictures/Screenshots
-else
-  saveLocation=~/Pictures/Screenshots
-  if [ ! -d $saveLocation ]; then
-    mkdir -p $saveLocation
-  fi
-fi
+file_name="Screenshot_$(date +%Y-%m-%d_%H%M%S).png"
+output_dir="$HOME/Pictures/Screenshot"
+mode="fullscreen"
 
-saveLocation=($saveLocation/Screenshot\ $(date +%Y-%m-%d)\ $(date +%H%M%S)\.png)
+function help() {
+  cat <<EOF
+Usage: $0 [options]
+Options:
+  -m, --mode <mode>        Specify the screenshot mode: fullscreen, region, window (default: fullscreen)
+  -o, --output-dir <dir>   Specify the output directory (default: ~/Pictures/Screenshot)
+  -n, --file-name <name>   Specify the file name (default: Screenshot_YYYY-MM-DD_HHMMSS.png)
+  -h, --help               Show this help message and exit
+EOF
+}
 
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 [--full | --select]"
-  echo ""
-  echo "  --full   : Capture a full screenshot."
-  echo "  --select : Capture a selected area screenshot."
-  exit 1
-fi
+function send_notify() {
+  notify-send "Screenshow saved" \
+    "Image saved in <i>${1}</i> and copied to clipboard." \
+    -i "${1}"
+}
 
-case $1 in
-"--full")
-  grim "$saveLocation"
-  ;;
-"--select")
-  grim -g "$(slurp)" "$saveLocation"
-  if [ $? -ne 0 ]; then
+function grab_window() {
+  local monitors=$(hyprctl -j monitors)
+  local clients=$(hyprctl -j clients | jq -r '[.[] | select(.workspace.id | contains('$(echo $monitors | jq -r 'map(.activeWorkspace.id) | join(",")')'))]')
+  local boxes="$(echo $clients | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1]) \(.title)"' | cut -f1,2 -d' ')"
+  slurp -r <<<"$boxes"
+}
+
+function take_screenshot() {
+  mkdir -p "$output_dir"
+
+  case "$mode" in
+  fullscreen)
+    grim "${output_dir}/${file_name}"
+    ;;
+  region)
+    grim -g "$(slurp -d)" "${output_dir}/${file_name}"
+    ;;
+  window)
+    grim -g "$(grab_window)" "${output_dir}/${file_name}"
+    ;;
+  *)
+    echo "Invalid mode: $mode"
     exit 1
-  fi
-  ;;
-esac
+    ;;
+  esac
 
-wl-copy <"$saveLocation"
+  wl-copy --type image/png <"${output_dir}/${file_name}"
+  send_notify "${output_dir}/${file_name}"
+}
 
-notify-send "Screenshot saved to $saveLocation"
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  -m | --mode)
+    mode="$2"
+    shift 2
+    ;;
+  -o | --output-dir)
+    output_dir="$2"
+    shift 2
+    ;;
+  -n | --file-name)
+    file_name="$2"
+    shift 2
+    ;;
+  -h | --help)
+    help
+    exit
+    ;;
+  *)
+    echo "Unknown option: $1"
+    exit 1
+    ;;
+  esac
+
+done
+
+take_screenshot "$mode" "$output_dir" "$file_name"
