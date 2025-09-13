6ed6f43879f5617735348298abed981a649bec1f
tiesen243 <ttien56906@gmail.com>
2025-09-14 02:15:25 +0700

    chore: minor changes

--- nvim/lua/plugins/lang/rust.lua
+++ nvim/lua/plugins/lang/rust.lua
@@ -1,19 +1,19 @@
 vim.lsp.enable("rust_analyzer")
 
-Yuki.format.register({
-  priority = 200,
-  name = "RustFmt",
-  active = function(bufnr)
-    local client = vim.lsp.get_clients({ name = "rust_analyzer", bufnr = bufnr })[1]
-    return client ~= nil
-  end,
-  command = function(bufnr)
-    local client = vim.lsp.get_clients({ name = "rust_analyzer", bufnr = bufnr })[1]
-    if client then
-      vim.cmd("RustFmt")
-    end
-  end,
-})
+-- Yuki.format.register({
+--   priority = 200,
+--   name = "RustFmt",
+--   active = function(bufnr)
+--     local client = vim.lsp.get_clients({ name = "rust_analyzer", bufnr = bufnr })[1]
+--     return client ~= nil
+--   end,
+--   command = function(bufnr)
+--     local client = vim.lsp.get_clients({ name = "rust_analyzer", bufnr = bufnr })[1]
+--     if client then
+--       vim.cmd("RustFmt")
+--     end
+--   end,
+-- })
 
 return {
   {
