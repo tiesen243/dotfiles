fc3d2cb2f39211fbd1f5a2e4a350d883317aa6bc
tiesen243 <ttien56906@gmail.com>
2025-09-21 13:56:27 +0700

    chore: minor changes

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -7,37 +7,37 @@ return {
     opts = { theme = "dark", transparent = true },
   },
 
-  {
-    "akinsho/bufferline.nvim",
-    enabled = false,
-    lazy = false,
-    keys = {
-      { "<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick Buffer" },
-      { "<leader>bP", "<cmd>BufferLinePickClose<cr>", desc = "Pick Close Buffer" },
-      { "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Close Left Buffers" },
-      { "<leader>bh", "<cmd>BufferLineCloseRight<cr>", desc = "Close Right Buffers" },
-    },
-    opts = {
-      highlights = function()
-        local status_ok, vercel = pcall(require, "vercel")
-        if not status_ok then
-          return {}
-        end
-        return vercel.highlights.bufferline
-      end,
-      options = {
-        diagnostics = "nvim_lsp",
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = " File Explorer",
-            highlight = "Directory",
-            text_align = "center",
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   enabled = false,
+  --   lazy = false,
+  --   keys = {
+  --     { "<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick Buffer" },
+  --     { "<leader>bP", "<cmd>BufferLinePickClose<cr>", desc = "Pick Close Buffer" },
+  --     { "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Close Left Buffers" },
+  --     { "<leader>bh", "<cmd>BufferLineCloseRight<cr>", desc = "Close Right Buffers" },
+  --   },
+  --   opts = {
+  --     highlights = function()
+  --       local status_ok, vercel = pcall(require, "vercel")
+  --       if not status_ok then
+  --         return {}
+  --       end
+  --       return vercel.highlights.bufferline
+  --     end,
+  --     options = {
+  --       diagnostics = "nvim_lsp",
+  --       offsets = {
+  --         {
+  --           filetype = "neo-tree",
+  --           text = " File Explorer",
+  --           highlight = "Directory",
+  --           text_align = "center",
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 
   {
     "nvim-lualine/lualine.nvim",
