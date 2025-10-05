419a0cae89d60a924c71e847b7a18621751cc078
tiesen243 <ttien56906@gmail.com>
2025-10-06 00:01:23 +0700

    feat: update neovim config

--- nvim/lua/yuki/init.lua
+++ nvim/lua/yuki/init.lua
@@ -5,20 +5,26 @@ M.utils = require("yuki.utils")
 M.format = require("yuki.format")
 
 M.configs = {
+  colorscheme = "default",
+  transparentEnable = false,
   icons = require("yuki.icons"),
   nesting_rules = require("yuki.nesting_rules"),
 }
 
----@param options {colorscheme: string, logo: string}
+---@param options {colorscheme: string, transparentEnable: boolean, logo: string}
 M.merge_config = function(options)
   M.configs = vim.tbl_deep_extend("force", M.configs, options or {})
 end
 
-M.setup = function()
-  -- Load core configuration modules
+--- @param lazyOpts table|nil
+M.setup = function(lazyOpts)
+  -- Load configuration
+  require("core.options")
+  require("lazy").setup(lazyOpts or {})
+
+  -- Load autocmd and keymaps
   require("core.autocmd")
   require("core.keymaps")
-  require("core.options")
 
   -- Apply colorscheme
   vim.cmd.colorscheme(M.configs.colorscheme)
