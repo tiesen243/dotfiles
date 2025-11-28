2d0db7ccd7d13f70187c503602f1d58ad65a159b
tiesen243 <ttien56906@gmail.com>
2025-11-29 00:03:21 +0700

    chore: minor changes

--- nvim/lua/plugins/lang/typescript.lua
+++ nvim/lua/plugins/lang/typescript.lua
@@ -98,7 +98,7 @@ return {
   {
     "mason-org/mason.nvim",
     optional = true,
-    opts = { ensure_installed = { "vtsls", "eslint-lsp", "prettierd" } },
+    opts = { ensure_installed = { "vtsls", "eslint-lsp", "prettier" } },
   },
 
   {
@@ -108,7 +108,6 @@ return {
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       for _, ft in ipairs(supported) do
         opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-        table.insert(opts.formatters_by_ft[ft], "prettierd")
         table.insert(opts.formatters_by_ft[ft], "prettier")
       end
     end,
