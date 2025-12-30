d39a462a17b2b136cff53537e933750c37f0b3ef
tiesen243 <ttien56906@gmail.com>
2025-12-30 21:01:22 +0700

    chore: minor changes

--- nvim/lua/plugins/lang/git.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = { ensure_installed = { "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" } },
-  },
-}
