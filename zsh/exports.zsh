445229987ae31257334e74490d1e8b4d5b9c231e
tiesen243 <ttien56906@gmail.com>
2026-03-05 14:36:38 +0700

    feat: update some configs

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,10 +1,10 @@
 export EDITOR="nvim"
 
 # >>> nvm initialize >>>
-if [ -d "$HOME/.nvm" ]; then
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+if [ -d "$HOME/.config/nvm" ]; then
+  export NVM_DIR="$HOME/.config/nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 fi
 # <<< nvm initialize <<<
 
