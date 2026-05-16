b4af98291ed17892e284869281dec471eda6ac40
tiesen243 <ttien56906@gmail.com>
2026-05-17 00:27:12 +0700

    chore: minor changes

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -97,22 +97,22 @@ return {
     keys = {
       -- stylua: ignore start
       -- Find
-      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-      { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
-      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
-      { "<leader>fi", function() Snacks.picker.icons() end, desc = "Icons" },
-      { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
-      { "<leader>fP", function() Snacks.picker.pick() end, desc = "Picker List" },
-      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+      { "<leader>fb", function() Snacks.picker.buffers() end, { desc = "Buffers" } },
+      { "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" } },
+      { "<leader>fg", function() Snacks.picker.grep() end, { desc = "Grep" } },
+      { "<leader>fi", function() Snacks.picker.icons() end, { desc = "Icons" } },
+      { "<leader>fp", function() Snacks.picker.projects() end, { desc = "Projects" } },
+      { "<leader>fP", function() Snacks.picker.pick() end, { desc = "Picker List" } },
+      { "<leader>fr", function() Snacks.picker.recent() end, { desc = "Recent" } },
       -- Git
-      { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-      { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse (open)" },
-      { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-      { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
-      { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-      { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-      { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-      { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+      { "<leader>gb", function() Snacks.picker.git_branches() end, { desc = "Git Branches" } },
+      { "<leader>gB", function() Snacks.gitbrowse() end, { desc = "Git Browse (open)" } },
+      { "<leader>gl", function() Snacks.picker.git_log() end, { desc = "Git Log" } },
+      { "<leader>gL", function() Snacks.picker.git_log_line() end, { desc = "Git Log Line" } },
+      { "<leader>gs", function() Snacks.picker.git_status() end, { desc = "Git Status" } },
+      { "<leader>gS", function() Snacks.picker.git_stash() end, { desc = "Git Stash" } },
+      { "<leader>gd", function() Snacks.picker.git_diff() end, { desc = "Git Diff (Hunks)" } },
+      { "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Log File" } },
       -- stylua: ignore end
     },
     opts = {
