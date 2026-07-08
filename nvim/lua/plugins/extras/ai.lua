5210a2aacf5d3a9cb24f89959ddab9d55d284613
tiesen243 <ttien56906@gmail.com>
2026-07-08 10:44:24 +0700

    chore: minor changes

--- nvim/lua/plugins/extras/ai.lua
+++ nvim/lua/plugins/extras/ai.lua
@@ -19,73 +19,73 @@ local M = {
     },
   },
 
-  {
-    { src = "https://github.com/CopilotC-Nvim/CopilotChat.nvim" },
-    keys = {
-      { "<c-s>", "<CR>", { ft = "copilot-chat", desc = "Submit Prompt", remap = true } },
-      { "<leader>a", "", { desc = "+ai", mode = { "n", "v" } } },
-      {
-        "<leader>aa",
-        function()
-          return require("CopilotChat").toggle()
-        end,
-        {
-          desc = "Toggle (CopilotChat)",
-          mode = { "n", "v" },
-        },
-      },
-      {
-        "<leader>ax",
-        function()
-          return require("CopilotChat").reset()
-        end,
-        {
-          desc = "Clear (CopilotChat)",
-          mode = { "n", "v" },
-        },
-      },
-      {
-        "<leader>aq",
-        function()
-          vim.ui.input({ prompt = "Quick Chat: " }, function(input)
-            if input ~= "" then
-              require("CopilotChat").ask(input)
-            end
-          end)
-        end,
-        {
-          desc = "Quick Chat (CopilotChat)",
-          mode = { "n", "v" },
-        },
-      },
-      {
-        "<leader>ap",
-        function()
-          require("CopilotChat").select_prompt()
-        end,
-        {
-          desc = "Prompt Actions (CopilotChat)",
-          mode = { "n", "v" },
-        },
-      },
-    },
-    opts = function()
-      local user = vim.env.USER or "User"
-      user = user:sub(1, 1):upper() .. user:sub(2)
-
-      return {
-        auto_insert_mode = true,
-        model = "gpt-5-mini",
-        question_header = "  " .. user .. " ",
-        answer_header = "  Copilot ",
-        sticky = { "#buffer" },
-        window = {
-          width = 0.4,
-        },
-        mappings = { reset = { normal = "" } },
-      }
-    end,
-  },
+  -- {
+  --   { src = "https://github.com/CopilotC-Nvim/CopilotChat.nvim" },
+  --   keys = {
+  --     { "<c-s>", "<CR>", { ft = "copilot-chat", desc = "Submit Prompt", remap = true } },
+  --     { "<leader>a", "", { desc = "+ai", mode = { "n", "v" } } },
+  --     {
+  --       "<leader>aa",
+  --       function()
+  --         return require("CopilotChat").toggle()
+  --       end,
+  --       {
+  --         desc = "Toggle (CopilotChat)",
+  --         mode = { "n", "v" },
+  --       },
+  --     },
+  --     {
+  --       "<leader>ax",
+  --       function()
+  --         return require("CopilotChat").reset()
+  --       end,
+  --       {
+  --         desc = "Clear (CopilotChat)",
+  --         mode = { "n", "v" },
+  --       },
+  --     },
+  --     {
+  --       "<leader>aq",
+  --       function()
+  --         vim.ui.input({ prompt = "Quick Chat: " }, function(input)
+  --           if input ~= "" then
+  --             require("CopilotChat").ask(input)
+  --           end
+  --         end)
+  --       end,
+  --       {
+  --         desc = "Quick Chat (CopilotChat)",
+  --         mode = { "n", "v" },
+  --       },
+  --     },
+  --     {
+  --       "<leader>ap",
+  --       function()
+  --         require("CopilotChat").select_prompt()
+  --       end,
+  --       {
+  --         desc = "Prompt Actions (CopilotChat)",
+  --         mode = { "n", "v" },
+  --       },
+  --     },
+  --   },
+  --   opts = function()
+  --     local user = vim.env.USER or "User"
+  --     user = user:sub(1, 1):upper() .. user:sub(2)
+  --
+  --     return {
+  --       auto_insert_mode = true,
+  --       model = "gpt-5-mini",
+  --       question_header = "  " .. user .. " ",
+  --       answer_header = "  Copilot ",
+  --       sticky = { "#buffer" },
+  --       window = {
+  --         width = 0.4,
+  --       },
+  --       mappings = { reset = { normal = "" } },
+  --     }
+  --   end,
+  -- },
 }
 
 if Yuki.configs.ai_cmp then
